f5fae027655897aa86d658ce333952f2efcb1d69
Thiago-PC <thiagofoz@gmail.com>
2018-09-04 13:20:45 -0300

    Terminando mobile

--- novo/js/index.js
+++
@@ -1,3 +0,0 @@
-/*
-9/22/2014 - Update 1: Added Author Name and changed some html and css. :) Enjoy.
-*/
---
+++ teste/js/index.js
@@ -0,0 +1,15 @@
+/*
+9/22/2014 - Update 1: Added Author Name and changed some html and css. :) Enjoy.
+*/
+
+function swipe(src) {
+    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=0,height=0,left=-1000,top=-1000`;
+    window.open(src,'Foto Equipe', params);
+    // var largeImage = document.getElementById('largeImage');
+    // largeImage.style.display = 'block';
+    // largeImage.style.width=200+"px";
+    // largeImage.style.height=200+"px";
+    // var url=largeImage.getAttribute('src');
+    // window.open(url,'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
+}
